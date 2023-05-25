9221f077de30cfedcc477be4a6d61324482a9df5
MWyssy <mike@wyss.co.uk>
2023-05-25 22:39:28 +0300

    add fix to user_data

--- modules/auto_scaling/user_data.sh
+++ modules/auto_scaling/user_data.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 sudo apt-get update
-sudo /usr/bin/git clone https://MWyssy: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@github.com/MWyssy/ce-terraform-project-html-host.git
+home=/home/ubuntu
+folder=$home/localfolder
+/usr/bin/git clone https://MWyssy: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@github.com/MWyssy/ce-terraform-project-html-host.git $folder
 sudo apt-get install nginx -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
 sudo rm /etc/nginx/sites-enabled/default
 sudo rm /usr/share/nginx/html/index.html
 sudo rm /etc/nginx/nginx.conf
-sudo mv ~/ce-terraform-project-html-host/index.html /usr/share/nginx/html/index.html
-sudo mv ~/ce-terraform-project-html-host/styles.css /usr/share/nginx/html/styles.css
-sudo mv ~/ce-terraform-project-html-host/nginx.conf /etc/nginx/nginx.conf
+cd /home/ubuntu/localfolder
+sudo mv index.html /usr/share/nginx/html/index.html
+sudo mv styles.css /usr/share/nginx/html/styles.css
+sudo mv nginx.conf /etc/nginx/nginx.conf
 sudo systemctl restart nginx.service
